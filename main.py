a2124e066e821d2e97f77441df4a0ab7db8dfb0c
daninewbs <51922543+daninewbs@users.noreply.github.com>
2021-11-29 14:17:21 -0500

    Update main.py
    now has buzzer that makes noise after sunrise sequence ends

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+#Danielle Newberry &  Ben Allan-Rahill
+#code to power sunrise lamp using esp32 feather huzzah
 
-from machine import Pin
+
+from machine import Pin, PWM, RTC
 import esp 
 import machine, neopixel
 import time
-
-
-
-####################################################
-#light code
 
 #constant colors
 BLUE =(100,149,237)
@@ -19,50 +17,81 @@ ORANGE = (205,55,0)
 RED = (205,0,0)
 
 LIGHTS = 24
+BUZZER_PIN = 25
+NEO_PIN = 26
 
 
+def sunrise():
+    np = neopixel.NeoPixel(Pin(NEO_PIN), LIGHTS) #using 24 pixel ring
+    esp.osdebug(None) #disable debugging
 
-np = neopixel.NeoPixel(Pin(26), LIGHTS) #using 24 pixel ring
-esp.osdebug(None) #disable debugging
+    #TODO: (later) check server see what color other light is at and (if diff) set this light to that color
+    #TODO: press putton on LCD Screen to cycle though color selection
 
-#TODO: (later) check server see what color other light is at and (if diff) set this light to that color
-#TODO: press putton on LCD Screen to cycle though color selection
-
-#set all neopixels to same color
-"""color = RED
-for i in range(LIGHTS):  
-    np[i] = color 
-np.write()
-
-
-# clear
-for i in range(LIGHTS):
-    np[i] = (0, 0, 0)
-np.write()
-"""
-
-# https://learn.adafruit.com/pyportal-wake-up-light?view=all
+    #set all neopixels to same color
+    """color = RED
+    for i in range(LIGHTS):  
+        np[i] = color 
+    np.write()
 
 
-#TODO: check RTC to see if 7AM
-default_wake_up = "7:30A"
+    # clear
+    for i in range(LIGHTS):
+        np[i] = (0, 0, 0)
+    np.write()
+    """
 
-# number of minutes it takes for strip to fade from min to max
-light_minutes = 30
+    # https://learn.adafruit.com/pyportal-wake-up-light?view=all
 
 
-for i in range(light_minutes - 1):
+    #TODO: check RTC to see if 7AM
+    default_wake_up = "7:30A"
+
+    # number of minutes it takes for strip to fade from min to max
+    light_minutes = 10 #30 mins
+
+
+    for i in range(light_minutes - 1):
+            
+            for j in range(LIGHTS):  
+                np[j] = (i,i//2,0) 
+                np.write()
+            
+            time.sleep(.5) # 60 for once per min
+            #light_minutes -= light_minutes
         
-        for j in range(LIGHTS):  
-            np[j] = (i,i//2,0) 
-            np.write()
-        
-        time.sleep(60) # 60 for once per min
-        #light_minutes -= light_minutes
-    
 
-#When user presses button lights off
+    #When ulights reach full brightness (at alarm time buzzer goes off)
 
 
-####################################################
-#buzzer code
+def alarm():
+    #buzzer code adapted from: https://micropython-on-wemos-d1-mini.readthedocs.io/en/latest/basics.html#beepers
+            
+    beeper = PWM(Pin(BUZZER_PIN), freq=440, duty=512)
+
+    tempo = 5
+    tones = {
+        'c': 262,
+        'd': 294,
+        'e': 330,
+        'f': 349,
+        'g': 392,
+        'a': 440,
+        'b': 494,
+        'C': 523,
+        ' ': 0,
+    }
+
+    melody = 'cdefgabC'
+    rhythm = [8, 8, 8, 8, 8, 8, 8, 8]
+
+    for tone, length in zip(melody, rhythm):
+        beeper.freq(tones[tone])
+        time.sleep(tempo/length)
+
+    beeper.deinit()
+    #repeat until user presses off button
+
+
+sunrise()
+alarm()
